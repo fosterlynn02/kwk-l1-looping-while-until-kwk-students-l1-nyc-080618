44ecb3ebf49e7d7c5c7096f10ec0a4632cf35c3e
fosterlynn02 <fosterlynnboulin@gmail.com>
2018-08-07 19:00:49 +0000

    automatically backed up by learn

--- Until.rb
+++ Until.rb
@@ -1,10 +1,10 @@
-def Taylor_Swift 
-  Taylor Swift = 0 
-  until Taylor Swift = 13 
-  puts "I love Taylor Swift"
-Taylor Swift += 1
+def taylor_swift 
+  taylor swift = 0 
+  until taylor swift = 13 
+  puts "I love taylor swift"
+taylor swift += 1
 
 end 
 end 
 
-Taylor_Swift
+taylor_swift
